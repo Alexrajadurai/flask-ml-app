b4844ba98271494994608a5cf2433a74ef2bcebb
Alex A <alexrajadurai007@gmail.com>
2025-04-22 02:18:36 -0700

    updated app.py

--- app/app.py
+++ app/app.py
@@ -18,9 +18,9 @@ def model_pred():
         text = request.form['text']
         random_str = uuid.uuid4().hex
         path = "static/" + random_str + ".svg"
-        model = load('model.joblib')
+        model = load('app/model.joblib')
         np_arr = float_to_np(text)
-        make_picture('AgesAndHeights.pkl', model, np_arr, path)
+        make_picture('app/AgesAndHeights.pkl', model, np_arr, path)
         return render_template('index.html', href = path)
         
 def make_picture(training_data, model, new_inp,output_file):
